7f3495ee8159c2c7840a6088f9b6d15117e17295
rahmadanu <sadewarahmadanu@gmail.com>
2022-09-22 18:24:29 +0700

    Configure login-register flow

---
+++ app/src/main/java/com/binar/notetaking/presentation/ui/home/HomeFragment.kt
@@ -0,0 +1,35 @@
+package com.binar.notetaking.presentation.ui.home
+
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.binar.notetaking.R
+import com.binar.notetaking.databinding.FragmentHomeBinding
+import com.binar.notetaking.presentation.ui.login.LoginFragment
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
